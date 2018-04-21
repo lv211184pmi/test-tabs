8d9a84dd834c1e8d75a0a21701a8d5b34d84a4d2
Mykhailo Panas <lv211184pmi@gmail.com>
2018-04-21 12:22:00 +0300

    implemented logic with HostListener and Renderer2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component, AfterViewInit, Renderer2, ElementRef, ViewChild } from '@angular/core';
-
-import { MatTabsModule } from '@angular/material/tabs';
+import { Component, 
+         AfterViewInit, 
+         ElementRef, 
+         ViewChild, 
+         HostListener,
+         Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +13,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 export class AppComponent implements AfterViewInit {
   @ViewChild('gamesRow') gamesRow: ElementRef;
   @ViewChild('gamesItem') gamesItem: ElementRef;
-  
+
   gamesList: any[] = [
     {game:'soccer'},
     {game:'volleyball'},
@@ -19,42 +22,53 @@ export class AppComponent implements AfterViewInit {
     {game:'basketball'}
   ];
 
-  moreGames: any[] = [{game: 'beliberda'}];
+  moreGames: any[] = [];
 
-  debounce(func, wait, immediate?) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      clearTimeout(timeout);
-      timeout = setTimeout(function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      }, wait);
-      if (immediate && !timeout) func.apply(context, args);
-    };
+  constructor(private renderer: Renderer2) {}
+
+  @HostListener('window:resize', ['$event'])
+    onResize(event) {
+      // let li = this.gamesItem.nativeElement.clientWidth,
+      //     ul = this.gamesRow.nativeElement.clientWidth,
+      //     games = this.gamesList.length + 1,
+      //     brakepoint1 = ul/(li*games),
+      //     brakepoint2 = ul - (li*games);
+      // if(brakepoint1 > 1) {
+      //   this.gamesList.push(this.moreGames.splice(0, 1)[0]);
+      // } else if(brakepoint2 < 0){
+      //   this.moreGames.unshift(this.gamesList.splice(-1, 1)[0]);
+      // }
+      return true;
+    }
+
+  debounce (fn, delay) {
+    var t;
+    return function () {
+      clearTimeout(t)
+      t = setTimeout(fn, delay)
+    }
   }
-
-  constructor(
-    private renderer: Renderer2) { }
 
   ngAfterViewInit() {
-    let rowWidth = this.gamesItem.nativeElement.parentElement.clientWidth;
-    let itemWidth = this.gamesItem.nativeElement.clientWidth;
-    let totalLength = () => itemWidth * this.gamesList.length + 200;
-    let divider = Math.ceil(totalLength()/rowWidth);
-    this.moreGames = this.gamesList.slice(divider);
-    this.gamesList = this.gamesList.splice(0,(this.gamesList.length-divider));
-    this.renderer.listen(window, 'resize', this.debounce((event) => {
-      let difference = totalLength() - this.gamesItem.nativeElement.parentElement.clientWidth;
-      if(this.gamesItem.nativeElement.parentElement.clientWidth > totalLength()){
-        this.gamesList.push(this.moreGames.splice(- 1, 1)[0]);
-      } else {
-        if(this.gamesItem.nativeElement.parentElement.clientWidth < totalLength()){
-          this.moreGames.push(this.gamesList.splice(0, 1)[0]);
-        }
-      }
-    }, 50));
-  }
+    let li = this.gamesItem.nativeElement.clientWidth,
+        ul = this.gamesRow.nativeElement.clientWidth,
+        divider =  Math.floor(ul/li) - 1;
 
-  
+    this.moreGames = this.gamesList.slice(divider);
+    this.gamesList = this.gamesList.splice(0, divider);
+
+    //for trying @HostListener comment this renderer and no debounce needed
+    this.renderer.listen(window, 'resize', this.debounce((event) => {
+      let li = this.gamesItem.nativeElement.clientWidth,
+            ul = this.gamesRow.nativeElement.clientWidth,
+            games = this.gamesList.length + 1,
+            brakepointRight = ul/(li*games),
+            brakepointLeft = ul - (li*games);
+        if(brakepointRight > 1) {
+          this.gamesList.push(this.moreGames.splice(0, 1)[0]);
+        } else if(brakepointLeft < 0){
+          this.moreGames.unshift(this.gamesList.splice(-1, 1)[0]);
+        }
+    }, 250));
+  }
 }
